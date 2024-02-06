d0c3498a770d2381b1ef328280c26b3d5cef791c
yanyunfang <411256509@qq.com>
2024-02-06 13:53:56 +0800

    init 更换包名，升级springboot3.0

---
+++ auths-common/auths-common-core/src/main/java/com/auths/common/core/utils/JsonUtils.java
@@ -0,0 +1,7 @@
+package com.auths.common.core.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtils {
+    public static final ObjectMapper objectMapper = new ObjectMapper();
+}
